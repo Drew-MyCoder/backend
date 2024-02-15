f59257b9db87dcd7bb03d6150606687565bb0ef1
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-02-15 06:08:10 +0000

    refactor: improvemment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import uvicorn
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from api.auth import crud
+from api.database import get_db
 
 
 app = FastAPI(title='SCB API', deescription='backend for prayer request and testimony', version='1.0.0')
@@ -8,6 +10,11 @@ app = FastAPI(title='SCB API', deescription='backend for prayer request and test
 @app.get("/")
 async def read_root():
     return ('Welcome to scb api')
+
+
+@app.get("/auth/users")
+async def get_users(db=Depends(get_db)):
+    return crud.read_users(db)
 
 if __name__ == '__main__':
     uvicorn.run('main:app', host='localhost', port=8000, reload=True) 
