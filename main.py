bb3b09cd4731c0fcb3506c01865a7ecf0f8fe57e
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-02-18 21:29:36 +0000

    chore: base added

--- main.py
+++ main.py
@@ -2,21 +2,22 @@ import uvicorn
 
 from fastapi import FastAPI, Depends
 from api.auth import crud
-from api.database import get_db
+from api.database import engine, Base
+from api.auth.router import router as auth_router
 
+
+Base.metadata.create_all(bind=engine) 
 
 app = FastAPI(title='SCB API', deescription='backend for prayer request and testimony', version='1.0.0')
+app.include_router(auth_router)
 
 @app.get("/")
 async def read_root():
     return ('Welcome to scb api')
 
 
-@app.get("/auth/users")
-async def get_users(db=Depends(get_db)):
-    return crud.read_users(db)
-
 if __name__ == '__main__':
-    uvicorn.run('main:app', host='localhost', port=8000, reload=True) 
+    uvicorn.run('main:app', host='localhost', port=8000, 
+    reload=True) 
 
 
