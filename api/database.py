7d13445b6381c6aa4430c284545fab553fe89955
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-03-12 03:40:56 +0000

    db: fully set up

--- api/database.py
+++ api/database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 
+load_dotenv()
 
 database_name = os.getenv("DATABASE_NAME")
 database_username = os.getenv("DATABASE_USERNAME")
@@ -10,7 +11,6 @@ database_password = os.getenv("DATABASE_PASSWORD")
 database_host = os.getenv("DATABASE_HOST")
 
 
-load_dotenv()
 
 
 database_url: str = f"postgresql://{database_username}:{database_password}@{database_host}/{database_name}"
